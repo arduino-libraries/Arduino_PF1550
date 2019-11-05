9cc3614809dc57935faa4ae5062c25ee7fcdffd9
Alexander Entinger <cto@lxrobotics.com>
2019-11-05 07:25:51 +0100

    Adding header file Arduino_PMIC.h which includes PF1550.h - the idea being that every pmic top level include header file is Arduino_PMIC

--- examples/BasicUsage/BasicUsage.ino
+++ examples/BasicUsage/BasicUsage.ino
@@ -1,20 +1,8 @@
-#include "PF1550.h"
+#include "Arduino_PMIC.h"
 
 void setup() {
   PMIC.begin();
-/*
-  PMIC.setLDO1Voltage(PF1550::Ldo1Voltage::V_1_00);
-  PMIC.turnLDO1On(PF1550::Ldo1Mode::Normal);
-  PMIC.turnLDO1On(PF1550::Ldo1Mode::Standby);
 
-  PMIC.setLDO2Voltage(PF1550::Ldo2Voltage::V_1_80);
-  PMIC.turnLDO2On(PF1550::Ldo2Mode::Normal);
-  PMIC.turnLDO2On(PF1550::Ldo2Mode::Standby);
-
-  PMIC.setLDO3Voltage(PF1550::Ldo3Voltage::V_1_20);
-  PMIC.turnLDO3On(PF1550::Ldo3Mode::Normal);
-  PMIC.turnLDO3On(PF1550::Ldo3Mode::Standby);
-*/
   PMIC.configLDO1(Ldo1Voltage::V_1_00, true, true, false);
   PMIC.configLDO2(Ldo2Voltage::V_1_80, true, true, false);
   PMIC.configLDO3(Ldo3Voltage::V_1_20, true, true, false);
